88fb8d6b4215021ec95773c87ef8584b66b3cc5e
Nina Mejač <nina.mejac@gmail.com>
2015-01-05 22:59:13 +0100

    Update ForestIdeals.hs

--- ForestIdeals.hs
+++ ForestIdeals.hs
@@ -8,5 +8,8 @@ simpleForest = [Simple.Tree 0 [Simple.Tree 1 []], Simple.Tree 2 [Simple.Tree 3 [
 advancedForest :: Advanced.Forest
 advancedForest = [Advanced.Tree 0 [Advanced.Tree 1 []], Advanced.Tree 2 [Advanced.Tree 3 [], Advanced.Tree 4 []]]
 
+forestIdealsSimple :: IO ()
 forestIdealsSimple = simpleForestIdealsA simpleForest
+
+forestIdealsAdvanced :: IO ()
 forestIdealsAdvanced = advancedForestIdealsA advancedForest
