243d4fc914343f742a1fadf912f7f40dd7acc8d7
Anže <an3180@student.uni-lj.si>
2015-01-07 20:08:30 +0100

    example for main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,10 @@ import ForestIdeals.Simple
 import ForestIdeals.Advanced
 import ForestIdeals.Tree
 
+-- | simpleForestIdeals generates all forest ideals of a given forest
+--
+-- >>> simpleForestIdeals
+-- [Tree 0 [Tree 1 []], Tree 2 [Tree 3 [], Tree 4 []]]
 simpleForestIdeals :: IO ()
 simpleForestIdeals = do
 	forestInput <- getLine
@@ -14,7 +18,10 @@ simpleForestIdeals = do
 		allStates = loop_forest drawIdeal forest state
 	putStrLn $ unlines $ printIdeal forest allStates
 
-
+-- | simpleForestIdeals generates all forest ideals of a given forest
+--
+-- >>> advancedForestIdeals
+-- [Tree 0 [Tree 1 []], Tree 2 [Tree 3 [], Tree 4 []]]
 advancedForestIdeals :: IO ()
 advancedForestIdeals = do
 	forestInput <- getLine
