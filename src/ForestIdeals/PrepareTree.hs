57a35e67f3041ee692072a6e53baf1bb31332e8d
Anže <an3180@student.uni-lj.si>
2015-01-07 19:19:23 +0100

    Docs for prepareTree

--- src/ForestIdeals/PrepareTree.hs
+++ src/ForestIdeals/PrepareTree.hs
@@ -3,23 +3,27 @@ module ForestIdeals.PrepareTree (prepareTree) where
 import ForestIdeals.Tree
 import Text.ParserCombinators.Parsec
 
--- parse node with subforest
+-- | parse node with subforest
 parseSubforest n = do
 	char '['
 	f <- many parseString
 	char ']'
 	return (Tree n f)
 
--- parse node without subforest
+-- | parse node without subforest
 parseNode :: Monad m => a -> m (Tree a)
 parseNode n = return (Tree n [])
 
--- parse string
+-- | parse string
 parseString :: Parser (Tree Char)
 parseString = do
 	n <- noneOf "[]"
 	((parseSubforest n) <|> (parseNode n))
 
+-- | prepare tree
+--
+-- >>> prepareTree "2[34]"
+-- Tree 2 [Tree 3 [],Tree 4 []]
 prepareTree :: IO ()
 prepareTree = do
 	tree <- getLine
