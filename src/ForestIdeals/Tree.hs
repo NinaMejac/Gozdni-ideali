6f6bb726e1f893bd34949aac05bfba81ba53c301
Nina Mejač <nina.mejac@gmail.com>
2015-01-07 19:16:49 +0100

    Update Tree.hs

--- src/ForestIdeals/Tree.hs
+++ src/ForestIdeals/Tree.hs
@@ -1,5 +1,10 @@
+-- | Module ForestIdeals.Tree is a module that generates forest or tree
 module ForestIdeals.Tree (Tree (..), Forest) where
 
--- kreiramo strukturo
+-- | A forest is a graph that is compound of trees.
+
+-- | A forest is represented as a list of trees.
+-- | Nodes of a tree are integers. They start with 0 and are increasing.
+
 data Tree = Tree Int Forest deriving (Show, Read)
 type Forest = [Tree]
